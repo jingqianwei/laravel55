5bbd2f1abee3b698a4d09ca197fead4ba3938b7d
jqw <jqw@qq.com>
2018-02-05 10:25:28 +0000

    11

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -8,6 +8,10 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
+
+    /**
+        指定数据表
+     */
     protected $table = 'users';
     /**
      * The attributes that are mass assignable.
