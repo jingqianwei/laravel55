9b95bfd5073eec9f614d69119adcf39aa023fdbc
jqw <jqw@qq.com>
2018-02-05 08:03:03 +0000

    Procfile for Heroku

--- config/app.php
+++ config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * phpstorm ide提示插件
+         */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+
     ],
 
     /*
