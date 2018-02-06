2e00eb0368f5824c2dbdd6fc75ea1e1ad5a6f61c
jqw <jqw@qq.com>
2018-02-06 05:44:41 +0000

    111

--- config/database.php
+++ config/database.php
@@ -81,7 +81,7 @@ return [
             'schema'   => 'public',
             'sslmode'  => 'prefer',
         ],
-        
+
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'host' => env('DB_HOST', 'localhost'),
